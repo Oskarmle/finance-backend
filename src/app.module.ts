ecf6f5e37bdedc6107e0074e0ac1849d3b693ec2
Oskar Eriksen <omle.eriksen@gmail.com>
2025-02-07 15:25:34 +0100

    postgresql db connection setup and unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let repository: jest.Mocked<Partial<Repository<Category>>>;
 
   beforeEach(async () => {
+    repository = {
+      find: jest.fn().mockResolvedValue([{ id: 1, name: 'Category 1' }]),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        {
+          provide: getRepositoryToken(Category),
+          useValue: repository,
+        },
+        CategoriesService,
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
   });
 
-  it('should be defined', () => {
+  it('Should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // Needs to be done
+  describe('Categories', () => {
+    it('should return all categories', async () => {
+      const categories = await service.findAll();
+      expect(categories).toEqual([{ id: 1, name: 'Category 1' }]);
+    });
+
+    it('should return a category by id', async () => {
+      const category = await service.findOne(1);
+      expect(category).toEqual({ id: 1, name: 'Category 1' });
+    });
   });
 });
